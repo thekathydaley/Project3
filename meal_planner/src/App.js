57262d416863d525cac48b4bdcb6a59604cff35a
thekathydaley <80797589+thekathydaley@users.noreply.github.com>
2021-06-28 11:05:17 -0400

    third commit

--- meal_planner/src/App.js
+++ meal_planner/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Header from './components/Header';
+import Main from "./components/Main";
+import Calories from "./components/Calories/Calories";
+import Exercise from "./components/Exercise/Exercise";
+import Recipes from "./components/Recipes/Recipes";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+        <Switch>
+        <Route exact path='/' component={Main} />
+        <Route exact path="/calories" component={Calories} />
+        <Route exact path="/exercise" component={Exercise} />
+        <Route exact path="/recipes" component={Recipes} />
+        </Switch>
+
+
+      </Router>
+
     </div>
   );
 }
